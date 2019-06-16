0d8555e1b1a8fd031c38010e73f5451583d09069
almgwary <almgwaryfci@gmail.com>
2019-06-17 01:00:24 +0200

    fix: socket is not defined

--- configs/socket.js
+++ configs/socket.js
@@ -3,11 +3,11 @@ module.exports = function (io) {
     io.on('connection', function(socket){
         console.log('a user connected');
         socket.broadcast.emit('hi');
-    });
 
+        socket.on('push', function(msg){
+            console.log('message: ' + msg);
+            io.emit('push', msg);
+        });
 
-    socket.on('push', function(msg){
-        console.log('message: ' + msg);
-        io.emit('push', msg);
     });
 };
