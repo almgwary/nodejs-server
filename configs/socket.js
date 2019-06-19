5df81d322cbfbfbc962cd1ac21a83704dead76fa
almgwary <almgwaryfci@gmail.com>
2019-06-19 03:24:52 +0200

    feat: socket simulate send FCM notification when user disconnect

--- configs/socket.js
+++ configs/socket.js
@@ -1,12 +1,20 @@
 module.exports = function (io) {
    
+    console.log('start using socket io')
     io.on('connection', function(socket){
         console.log('a user connected');
-        socket.broadcast.emit('hi');
+        let {query} = socket.handshake;
+        let {name} = query ;
+        socket.broadcast.emit(`${name} connected`);
 
         socket.on('push', function(msg){
             console.log('message: ' + msg);
             io.emit('push', msg);
+        });
+
+
+        socket.on('disconnect', function(){
+            console.log(`user ${name} disconnected should send notification`);
         });
 
     });
